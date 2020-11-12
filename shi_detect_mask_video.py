891162f2fb493d9dd072a54680611652efe0baa6
lengkujiaai <lengkujiaai@126.com>
2020-11-12 10:58:51 +0800

    Update shi_detect_mask_video.py

--- shi_detect_mask_video.py
+++ shi_detect_mask_video.py
@@ -1,3 +1,5 @@
+#author: lengkujiaai@126.com
+
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
